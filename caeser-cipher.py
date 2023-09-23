d6791655306032847c677168e6113023f4d0d555
Kanishk Shrivastava <65497298+knixk@users.noreply.github.com>
2023-09-23 14:27:13 +0530

    Add files via upload

---
+++ caeser-cipher.py
@@ -0,0 +1,58 @@
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+#TODO-1: Create a function called 'encrypt' that takes the 'text' and 'shift' as inputs.
+
+def caeser(input_text, shift_no, direction):
+
+	ans = ""
+
+	for letter in input_text:
+		if (letter == " "):
+			ans += letter
+			continue
+
+		position_of_letter = alphabet.index(letter)
+
+		temp_shift_no = shift_no
+
+		if (direction == "decode"):
+			temp_shift_no *= -1
+
+		shift_position = position_of_letter + temp_shift_no
+
+		ans += alphabet[shift_position]
+
+	print(ans)
+
+    #TODO-2: Inside the 'encrypt' function, shift each letter of the 'text' forwards in the alphabet by the shift amount and print the encrypted text.  
+    #e.g. 
+    #plain_text = "hello"
+    #shift = 5
+    #cipher_text = "mjqqt"
+    #print output: "The encoded text is mjqqt"
+
+    ##HINT: How do you get the index of an item in a list:
+    #https://stackoverflow.com/questions/176918/finding-the-index-of-an-item-in-a-list
+
+    ##🐛Bug alert: What happens if you try to encode the word 'civilization'?🐛
+
+#TODO-3: Call the encrypt function and pass in the user inputs. You should be able to test the code and encrypt a message. 
+# encrypt(text, shift)
+
+while (True):
+	
+	direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+
+	text = input("Type your message:\n").lower()
+
+	shift = int(input("Type the shift number:\n"))
+
+	caeser(text, shift, direction)		
+
+	again = input("Would you like to go again? (y / n)")
+
+	if (again == "n"):
+		break;
+		
+
+
