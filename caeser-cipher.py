4e8426251dd3f26c3bbfe26948515e1af5d64cf3
Kanishk Shrivastava <shrivastavakanishk3@gmail.com>
2023-09-25 12:27:26 +0530

    changes

--- caeser-cipher.py
+++ caeser-cipher.py
@@ -1,3 +1,6 @@
+from art import logo
+print(logo)
+
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
 #TODO-1: Create a function called 'encrypt' that takes the 'text' and 'shift' as inputs.
@@ -52,7 +55,7 @@ while (True):
 	again = input("Would you like to go again? (y / n)")
 
 	if (again == "n"):
-		break;
+		break
 		
 
 
