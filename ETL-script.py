75d70a54ded1a7816f2d5aa5b075774e15132f0d
Kanishk Shrivastava <65497298+knixk@users.noreply.github.com>
2023-10-12 11:39:22 +0530

    Add files via upload

--- ETL-script.py
+++ ETL-script.py
@@ -1,20 +1,73 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[126]:
-
 import pandas as pd
-import numpy as np 
+# import numpy as np 
 import datetime 
+import pymysql
+import mysql.connector 
+from sqlalchemy import create_engine, types
 
+# Make connection
+database = "demo"
+# url = 123
+host = "localhost"
+username = "root"
+password = ""
 
-# In[89]:
+# Create the connection object   
+# connection = mysql.connector.connect(host = <host-name> , user = <username> , passwd = <password> )  
+myconn = mysql.connector.connect(host = "localhost", user = "root", passwd = "", database = "demo")  
+
+# engine = create_engine("mysql+pymysql://" + username + ":" + password + "@" + host + "/" + database)
+# df.to_sql('SData', con = engine, if_exists = 'append',index = False, chunksize = 1000)
+
+#printing the connection object   
+print(myconn)  
+
+#creating the cursor object  
+cur = myconn.cursor()  
+  
+print(cur)
+
+# try:  
+#     myconn = mysql.connector.connect(host = "localhost", user = "root", passwd = "", database = "demo")  
+#     cur = myconn.cursor()  
+
+# except:  
+#     print("Error connecting to database, check credentials.")
+
+# try:  
+#     dbs = cur.execute("show databases")  
+#     # cur.execute("""
+#     # CREATE TABLE `products` (
+# 	# `Order ID` TEXT(256) NOT NULL,
+# 	# `Product` TEXT(256) NOT NULL,
+# 	# `Quantity Ordered` TEXT,
+# 	# `Index` INT NOT NULL AUTO_INCREMENT,
+# 	# `Price Each` FLOAT,
+# 	# `Order Date` DATETIME,
+# 	# `Purchase Address` TEXT,
+# 	# `total_sales` FLOAT,
+# 	# `Month` INT,
+# 	# `Year` INT
+#     # );
+#     # """)
+#     # cur.execute("CREATE DATABASE demo")
+
+# except:  
+#     myconn.rollback()  
+# for x in cur:  
+#     print(x)  
+# myconn.close()  
+
+file_name = 'Updated_sales_data.csv'
+
 def extract():
-    df = pd.read_csv('Updated_sales_data.csv')
+    df = pd.read_csv(file_name)
     return df
 
 
-# In[90]:
 
 def transform(df):
     # Finding any null values
@@ -36,7 +89,11 @@ def transform(df):
 
     # Calculating total sales
 
-    cleaned['total_sales'] = cleaned['Quantity Ordered'].astype(int) * cleaned['Price Each'].astype(float)
+    cleaned['Quantity Ordered'] = cleaned['Quantity Ordered'].astype(int)
+    cleaned['Price Each'] = cleaned['Price Each'].astype(float)
+    cleaned['total_sales'] = cleaned['Quantity Ordered'] * cleaned['Price Each']
+
+    # cleaned['total_sales'] = cleaned['Quantity Ordered'].astype(int) * cleaned['Price Each'].astype(float)
 
     # removing duplicated indexes
     cleaned.drop(columns=cleaned.columns[0], axis=1, inplace=True)
@@ -65,8 +122,28 @@ def load(cleaned, total_sales_by_product):
     cleaned.to_csv('cleaned_data.csv')
     total_sales_by_product.to_csv('products_sale_by_month.csv')
 
+def insertIntoDB(df, table_name, myconn):
+    # df.to_sql(table_name, myconn, if_exists='replace', index=False)
 
+    engine = create_engine("mysql+pymysql://" + username + ":" + password + "@" + host + "/" + database)
+    df.to_sql(table_name, con = engine, if_exists = 'replace', index = False, chunksize = 1000)
+
+    # read the data
+    # df = pd.read_sql("SELECT * FROM table_name", conn)
+
+
+# ------------------------x--------------------------x------------------------x---------------------
 # ETL Pipeline
+
+# 1. extract
 df = extract()
+
+# 2. transform
 cleaned, total_sales_by_product = transform(df)
-load()
+
+# 3. load
+load(cleaned, total_sales_by_product)
+
+# inserting table with values from csv
+insertIntoDB(cleaned, "products", myconn)
+
