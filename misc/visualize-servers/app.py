d0c2a78157558be84a15ee83d2fb96ab5731e581
Kanishk Shrivastava <shrivastavakanishk3@gmail.com>
2024-09-27 13:07:05 +0530

    changes

--- misc/visualize-servers/app.py
+++ misc/visualize-servers/app.py
@@ -16,3 +16,38 @@ final_url = urlparse(url).netloc
 print(final_url)
 # facebook.com
 
+
+
+with open('history_data.txt', 'r') as f:
+    data = f.readlines()
+
+domain_names = set()
+
+for url in data:
+    final_url = urlparse(url).netloc
+#     print(final_url)
+    final_url = final_url.split(':')[0]
+#     print(final_url)
+    domain_names.add(final_url)
+
+for item in domain_names:
+    print(item)
+    
+    
+import socket
+ip_addr = socket.gethostbyname('google.nl')
+print(ip_addr)
+
+
+
+ip_set = set()
+
+for domain in domain_names:
+    try:
+        ip_addr = socket.gethostbyname(domain)
+        ip_set.add(ip_addr)
+    except:
+        print(domain)
+        
+print(ip_set)
+
