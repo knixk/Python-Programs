ac1a20fb74c4d8043f44877c883b6c9cc64522d8
Kanishk Shrivastava <shrivastavakanishk3@gmail.com>
2024-10-03 17:40:47 +0530

    changes

--- misc/turtle/main.py
+++ misc/turtle/main.py
@@ -1,17 +1,19 @@
 from turtle import Turtle as t, Screen as s
 import math
+from random import choice, randint
+
+colors = ["#E0FFFF", "red", "blue", "green", "purple", "cyan", "black"]
 
 my_screen = s()
 tt = t()
 
-tt.color('red')
 
 def draw_square():
     for i in range(0, 5):
         tt.fd(100)
         tt.rt(90)
 
-tt.color('red')
+# tt.color('#E0FFFF')
 # draw_square()
 
 def create_dashed_line():
@@ -20,7 +22,6 @@ def create_dashed_line():
         tt.forward(10)
         tt.pu()
         tt.forward(10)
-
 
 def draw_polygon(n):
     # tt.setposition(10, 10)
@@ -32,10 +33,24 @@ def draw_polygon(n):
         tt.rt(angle)
 
 # create_dashed_line()    
-
 # draw_polygon(10)
 
-for i in range (3, 11):
+# for i in range (3, 11):
+    random_color = choice(colors)
+    tt.color(random_color)
     draw_polygon(i)
+
+def random_walk(steps):
+        
+    tt.pensize(10)
+    
+    for _ in range(steps):
+        color = choice(colors)
+        tt.color(color)
+        angle = randint(0, 361) 
+        distance = randint(0, 100)
+        tt.fd(distance)
+        tt.right(angle)
+random_walk(100)
 
 my_screen.exitonclick()
