374e690f2b141ac0f2b3756d3263acaac757659b
Kanishk Shrivastava <shrivastavakanishk3@gmail.com>
2025-03-07 00:08:34 +0530

    changes

---
+++ misc/programs/hello.py
@@ -0,0 +1,11 @@
+# Q. print a string with a new line char
+
+# print("hello gamer \n Hello world")
+
+# Q. concat two strings
+name = "Kanishk"
+surname = "Shrivastava"
+fullname = name + "\n" + surname
+print(fullname)
+
+
